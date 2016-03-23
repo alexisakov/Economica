47884da5214f4d8b2bf59f705d4911726f207efd
alexisakov <ale.isakov@gmail.com>
2016-03-23 18:59:40 +0300

    Added files via upload

---
+++ Economica/Kernel/misc/project.m
@@ -0,0 +1,17 @@
+(* Wolfram Language package *)
+
+{
+        "author" ->
+           {
+               "name" -> "Alex Isakov",
+               "email" -> "ale.isakov@gmail.com",
+               "url" -> "http://www.alexisakov.com"
+           },           
+        "name" -> "Economica",
+        "version" -> "1.0",
+        "mathematica_version" -> "10.0+",
+        "dependencies" -> {},
+        "description"-> "Simple web installer for Mathematica projects",
+        "url" -> "https://github.com/alexisakov/Economica",
+        "license" ->  "MIT"
+}
