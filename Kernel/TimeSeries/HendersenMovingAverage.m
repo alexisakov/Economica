0ddb2c74f910fd25cb8d69a358f37ee225734008
alexisakov <ale.isakov@gmail.com>
2016-03-31 09:18:47 +0300

    Added files via upload

---
+++ Kernel/TimeSeries/HendersenMovingAverage.m
@@ -0,0 +1,29 @@
+(* :Title: HendersenMovingAverage *)
+
+(* :Author: Alex Isakov*)
+
+(* :Summary:
+See: 
+Office for National Statistics (2006), Fitting trends to time series data, http://goo.gl/S6UfHh *)
+
+
+BeginPackage["Economica`"]
+
+HendersenMovingAverage::usage = "";
+
+Begin["`Private`"];
+
+HendersenMovingAverage[l_, m_] := Module[
+  {w = (315 ((m + 1)^2 - #^2) ((m + 2)^2 - #^2) ((m + 
+           3)^2 - #^2) (3 (m + 2)^2 - 11 #^2 - 16))/(
+      8 (m + 2) ((m + 2)^2 - 1) (4 (m + 2)^2 - 1) (4 (m + 2)^2 - 
+         9) (4 (m + 2)^2 - 25)) & /@ Range[-m, m]
+   },
+  
+  MovingAverage[l, w]
+  
+  ]
+
+End[];
+EndPackage[];
+
