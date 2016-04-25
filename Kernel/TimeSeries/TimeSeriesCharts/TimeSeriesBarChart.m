7e458b1faa607eeaa423e4b9f146a9f040cf5d07
alexisakov <ale.isakov@gmail.com>
2016-04-25 18:27:27 +0300

    Added files via upload

--- Kernel/TimeSeries/TimeSeriesCharts/TimeSeriesBarChart.m
+++ Kernel/TimeSeries/TimeSeriesCharts/TimeSeriesBarChart.m
@@ -18,14 +18,37 @@ Options[TimeSeriesBarChart] = Join[Options[BarChart],
     mts = TimeSeries[{dts, vals}\[Transpose]];
     
     (*constructing the labels*)
-    labels = 
-     DateRange[First@dts, Last@dts, OptionValue[DateLabelStep]];
-    ticks = 
-     If[MemberQ[labels, #], 
-        VTBTextStyle@DateString[#, OptionValue[DateTicksFormat]], 
-        ""] & /@ dts;
+    labels = DateRange[First@dts, Last@dts, OptionValue[DateLabelStep]];
+    ticks = If[MemberQ[labels, #], VTBTextStyle@DateString[#, OptionValue[DateTicksFormat]], ""] & /@ dts;
     BarChart[mts,
      ChartLabels -> {ticks, None},
      Sequence @@ ((# -> OptionValue[#]) & /@ opt)]
+    ]
+  ];
+  
+  With[{opt = First /@ Options[BarChart]},
+	TimeSeriesBarChart[ts_List, tsLine_List, OptionsPattern[]] := Module[{
+		vals = (#["Values"] & /@ ts)\[Transpose],
+		dts = ts[[1]]["Dates"],
+		mts, labels, ticks},
+		mts = TimeSeries[{dts, vals}\[Transpose]];
+       (*constructing the labels*)
+		labels = DateRange[First@dts, Last@dts, OptionValue[DateLabelStep]];
+		ticks = If[MemberQ[labels, #], 
+        VTBTextStyle@DateString[#, OptionValue[DateTicksFormat]],""] & /@ dts;
+		
+		(*make the lines*)
+		
+		epiColor = ColorData["Rainbow"] /@ Range[Length@ts + 1, Length@ts + Length@tsLine];
+		epiLen = Length@((First@tsLine)["Dates"]);
+		epiLines = (Line[{Range@epiLen,  #["Values"]}\[Transpose]])&/@tsLine;
+		epilog=Riffle[epiColor,epiLines];
+	    epilog=Prepend[epilog,Thickness[0.004]];
+		epilog=Join[{Thickness[0.006],White,epiLines},epilog];
+		
+		BarChart[mts,
+			ChartLabels -> {ticks, None}, 
+			Epilog->epilog,
+			Sequence @@ ((# -> OptionValue[#]) & /@ opt)]
     ]
   ];
