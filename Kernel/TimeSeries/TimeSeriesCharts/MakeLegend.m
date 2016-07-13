cf4317381dd7285511ee1feaad3b201718ef61cd
alexisakov <ale.isakov@gmail.com>
2016-07-13 17:31:39 +0300

    Add files via upload

--- Kernel/TimeSeries/TimeSeriesCharts/MakeLegend.m
+++ Kernel/TimeSeries/TimeSeriesCharts/MakeLegend.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 (*  box1 = Graphics[{VTBBlue, Rectangle[]}, ImageSize -> 16];
  box2 = Graphics[{VTBViolet, Rectangle[]}, ImageSize -> 16];
 text1 = VTBTextStyle@voc["investment, pp"];
@@ -15,22 +17,21 @@ legend = Grid[
   
   
   
-Export[FileNameJoin[{exportDirectory, "pic.pdf"}], %]*)
-
-
+  Export[FileNameJoin[{exportDirectory, "pic.pdf"}], %]*)
+ 
+ClearAll@MakeLegend;
 Options[MakeLegend]={BaseStyle -> Directive[FontFamily -> "Arial", FontSize -> 22/1.5],FontSize->22/1.5};
 
-  
 MakeLegend[list_,OptionsPattern[]] := Module[
   {icons, text, legend},
   icons = MapIndexed[
-	Graphics[{PointSize[Large], Thickness[0.009],
+	Graphics[{PointSize[Large], Thickness[0.1],
 		If[Length@#1 == 1, ColorData["Rainbow", First@#2], Last@#1],
 		Which[
 			First@#1 == "Box", Rectangle[],
 			First@#1 == "Line", Line[{{0, 0.5}, {1, 0.5}}],
 			First@#1 == "Dot", Point[{0, 0}]]},
-		ImageSize -> 16] &,
+		ImageSize -> 24] &,
 	list[[All, 2 ;;]]];
 
   text = VTBTextStyle[#,FontSize->OptionValue[FontSize]]& /@ list[[All, 1]];
