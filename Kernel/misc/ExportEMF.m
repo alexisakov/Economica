6fc986ffae273da4be870502d39a42558d981b4f
alexisakov <ale.isakov@gmail.com>
2016-09-09 11:24:58 +0300

    Add files via upload

--- Kernel/misc/ExportEMF.m
+++ Kernel/misc/ExportEMF.m
@@ -3,11 +3,24 @@ BeginPackage["Economica`"]
 (* Exported symbols added here with SymbolName::usage *)  
 
 ExportEMF::usage="ExportEMF";
+ExportEPS::usage="ExportEPS";
+
 
 Begin["`Private`"] (* Begin Private Context *) 
 
 
 Options[ExportEMF] = Options[Export];
+Options[ExportEPS] = Options[Export];
+
+
+
+(*The Verbeia solution: http://mathematica.stackexchange.com/a/65551/128
+	The resulting charts are ugly though*)
+(*ExportEMF[pathorfile_String, obj_, opts : OptionsPattern[{Export}]] := With[
+{stringtouse = If[ToLowerCase[StringTake[pathorfile,-4]]===".emf", pathorfile, pathorfile<>".emf"]},
+	Export[stringtouse, 
+	DeleteCases[ obj /. {_Opacity, p_Point} :> {PointSize[0], p}, _Opacity, Infinity], opts] ]
+*)
 
 
 With[{opt = First /@ Options[Export]},
@@ -19,6 +32,17 @@ With[{opt = First /@ Options[Export]},
     	]
     ]
   ];
+  
+With[{opt = First /@ Options[Export]},
+	ExportEPS[filename_String, expr_, OptionsPattern[]] := Module[{
+    pic = First@ImportString[ExportString[expr, "PDF"]]},
+    Export[filename <> ".eps", pic, 
+    	Sequence @@ ((# -> OptionValue[#]) & /@ opt),
+    	ImageResolution -> 4100
+    	]
+    ]
+  ];
+  
 
 
 End[] (* End Private Context *)
