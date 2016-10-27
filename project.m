e3d7a8cbc284be7f63be90e3ebe80335df6eb7d5
alexisakov <ale.isakov@gmail.com>
2016-10-27 15:33:36 +0300

    Update project.m

--- project.m
+++ project.m
@@ -8,7 +8,7 @@
                "url" -> "http://www.alexisakov.com"
            },           
         "name" -> "Economica",
-        "version" -> "1.0",
+        "version" -> "1.1",
         "mathematica_version" -> "10.0+",
         "dependencies" -> {},
         "description"-> "Simple web installer for Mathematica projects",
