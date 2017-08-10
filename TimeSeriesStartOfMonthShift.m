e73416813723332be44194b62452fae217d65a83
alexisakov <ale.isakov@gmail.com>
2017-08-10 15:03:28 +0300

    Delete TimeSeriesStartOfMonthShift.m

--- TimeSeriesStartOfMonthShift.m
+++
@@ -1,7 +0,0 @@
-TimeSeriesStartOfMonthShift[ts_] := Module[{
-   dates = ts["Dates"], values = ts["Values"],
-   datelist},
-  datelist = (DateList /@ dates)[[All, 1 ;; 3]];
-  datelist[[All, 3]] = 1;
-  TimeSeries[Transpose[{datelist, values}]]
-  ]
