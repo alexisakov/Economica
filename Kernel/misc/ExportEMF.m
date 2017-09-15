f5bb7736339505031ba456d7dd60470b713e136f
alexisakov <ale.isakov@gmail.com>
2017-09-15 15:09:27 +0300

    Add files via upload

--- Kernel/misc/ExportEMF.m
+++ Kernel/misc/ExportEMF.m
@@ -1,3 +1,5 @@
+(* ::Package:: *)
+
 (* Wolfram Language package *)
 BeginPackage["Economica`"]
 (* Exported symbols added here with SymbolName::usage *)  
@@ -9,13 +11,7 @@ ExportEPS::usage="ExportEPS";
 Begin["`Private`"] (* Begin Private Context *) 
 
 
-<<<<<<< HEAD
-Options[ExportEMF] = Join[Options[Export],{BarChartTrick->False}];
-Options[ExportEPS] = Options[Export];
-
-
-=======
-Options[ExportEMF] = Options[Export];
+Options[ExportEMF] = Join[Options[Export],{BarChartQ->False}];
 Options[ExportEPS] = Options[Export];
 
 
@@ -27,27 +23,18 @@ Options[ExportEPS] = Options[Export];
 	Export[stringtouse, 
 	DeleteCases[ obj /. {_Opacity, p_Point} :> {PointSize[0], p}, _Opacity, Infinity], opts] ]
 *)
->>>>>>> origin/master
-
-(*The Verbeia solution: http://mathematica.stackexchange.com/a/65551/128
-	The resulting charts are ugly though*)
-(*ExportEMF[pathorfile_String, obj_, opts : OptionsPattern[{Export}]] := With[
-{stringtouse = If[ToLowerCase[StringTake[pathorfile,-4]]===".emf", pathorfile, pathorfile<>".emf"]},
-	Export[stringtouse, 
-	DeleteCases[ obj /. {_Opacity, p_Point} :> {PointSize[0], p}, _Opacity, Infinity], opts] ]
-*)
 
 
 With[{opt = First /@ Options[Export]},
-	ExportEMF[filename_String, expr_, OptionsPattern[]] := Module[{pic },
-	If[OptionValue[BarChartTrick]
-		, pic = expr/. _Opacity -> (## &[]);
-			Export[filename <> ".emf", pic, Sequence @@ ((# -> OptionValue[#]) & /@ opt)]
-		, pic = First@ImportString[ExportString[expr, "PDF"]];
-			Export[filename <> ".emf", pic, Sequence @@ ((# -> OptionValue[#]) & /@ opt), ImageResolution -> 4100]
-		]  
+	ExportEMF[filename_String, expr_, OptionsPattern[]] := Module[{
+	pic = First@ImportString[ExportString[expr, "PDF"]]},
+	If[OptionValue[BarChartQ],
+    Export[filename <> ".emf",expr/.{Opacity[0],__}->{}],
+    Export[filename <> ".emf", pic, ImageResolution -> 4100, Sequence @@ ((# -> OptionValue[#]) & /@ opt)
+    	]
     ]
-];
+  ]
+  ];
   
 With[{opt = First /@ Options[Export]},
 	ExportEPS[filename_String, expr_, OptionsPattern[]] := Module[{
@@ -59,19 +46,6 @@ With[{opt = First /@ Options[Export]},
     ]
   ];
   
-<<<<<<< HEAD
-=======
-With[{opt = First /@ Options[Export]},
-	ExportEPS[filename_String, expr_, OptionsPattern[]] := Module[{
-    pic = First@ImportString[ExportString[expr, "PDF"]]},
-    Export[filename <> ".eps", pic, 
-    	Sequence @@ ((# -> OptionValue[#]) & /@ opt),
-    	ImageResolution -> 4100
-    	]
-    ]
-  ];
-  
->>>>>>> origin/master
 
 
 End[] (* End Private Context *)
