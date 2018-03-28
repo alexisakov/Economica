60d7d4f7c3c5f0ed0626af806cdd0d72fe575963
alexisakov <ale.isakov@gmail.com>
2018-03-28 14:06:08 +0300

    Add files via upload

--- Kernel/misc/ExportEMF.m
+++ Kernel/misc/ExportEMF.m
@@ -16,22 +16,19 @@ Options[ExportEPS] = Options[Export];
 
 
 
-(*The Verbeia solution: http://mathematica.stackexchange.com/a/65551/128
-	The resulting charts are ugly though*)
-(*ExportEMF[pathorfile_String, obj_, opts : OptionsPattern[{Export}]] := With[
-{stringtouse = If[ToLowerCase[StringTake[pathorfile,-4]]===".emf", pathorfile, pathorfile<>".emf"]},
-	Export[stringtouse, 
-	DeleteCases[ obj /. {_Opacity, p_Point} :> {PointSize[0], p}, _Opacity, Infinity], opts] ]
-*)
+(*The Verbeia solution: http://mathematica.stackexchange.com/a/65551/128 The resulting charts are ugly though*)
+(*ExportEMF[pathorfile_String, obj_, opts : OptionsPattern[{Export}]] := With[ {stringtouse = If[ToLowerCase[StringTake[pathorfile,-4]]===".emf", pathorfile, pathorfile<>".emf"]},
+	Export[stringtouse, DeleteCases[ obj /. {_Opacity, p_Point} :> {PointSize[0], p}, _Opacity, Infinity], opts] ]*)
 
 
 With[{opt = First /@ Options[Export]},
 	ExportEMF[filename_String, expr_, OptionsPattern[]] := Module[{
 	pic = First@ImportString[ExportString[expr, "PDF"]]},
-	If[OptionValue[BarChartQ],
-    Export[filename <> ".emf",expr/.{Opacity[0],__}->{}],
-    Export[filename <> ".emf", pic, ImageResolution -> 4100, Sequence @@ ((# -> OptionValue[#]) & /@ opt)
-    	]
+	If[
+    OptionValue[BarChartQ],
+    (*if this is a bar chart then we attempt to use a W10 installation to export it in the background*)  
+        pic,
+        Export[filename <> ".emf", pic, ImageResolution -> 4100, Sequence @@ ((# -> OptionValue[#]) & /@ opt)]
     ]
   ]
   ];
