05b9229065056c8772127c7e7d1d89c7123784b1
alex isakov <ale.isakov@gmail.com>
2019-04-01 10:42:10 +0300

    Add files via upload

--- export/ExportEMF.m
+++ export/ExportEMF.m
@@ -4,22 +4,29 @@ Package["Economica`"]
 ExportEMF::usage="ExportEMF";
 ExportEPS::usage="ExportEPS";
 
-Options[ExportEMF] = Join[Options[Export],{BarChartQ->False}];
+Options[ExportEMF] = Join[Options[Export],{BarChartQ->False, ImageResolution -> 4100}];
 Options[ExportEPS] = Options[Export];
 
 (*The Verbeia solution: http://mathematica.stackexchange.com/a/65551/128 The resulting charts are ugly though*)
-(*ExportEMF[pathorfile_String, obj_, opts : OptionsPattern[{Export}]] := With[ {stringtouse = If[ToLowerCase[StringTake[pathorfile,-4]]===".emf", pathorfile, pathorfile<>".emf"]},
+(*ExportEMF[pathorfile_String, obj_, opts : OptionsPattern[{Export}]] := With[
+ {stringtouse = If[ToLowerCase[StringTake[pathorfile,-4]]===".emf", pathorfile, pathorfile<>".emf"]},
 	Export[stringtouse, DeleteCases[ obj /. {_Opacity, p_Point} :> {PointSize[0], p}, _Opacity, Infinity], opts] ]*)
 
 
 With[{opt = First /@ Options[Export]},
 	ExportEMF[filename_String, expr_, OptionsPattern[]] := Module[{
-	pic = First@ImportString[ExportString[expr, "PDF"]]},
-	If[
-    OptionValue[BarChartQ],
-    (*if this is a bar chart then we attempt to use a W10 installation to export it in the background*)  
-        pic,
-        Export[filename <> ".emf", pic, ImageResolution -> 4100, Sequence @@ ((# -> OptionValue[#]) & /@ opt)]
+		pic = First@ImportString[ExportString[expr, "PDF"]],
+		picO = expr /. _Opacity -> (## &[])
+		},
+	
+		Switch[OptionValue[BarChartQ],
+    		True,
+	    		(*if this is a bar chart then we attempt to use a W10 installation to export it in the background*)  
+	        	pic,
+        	False,
+      			Export[filename <> ".emf", pic, ImageResolution -> OptionValue[ImageResolution], Sequence @@ ((# -> OptionValue[#]) & /@ opt)],
+	        "OpacityTrick",
+      			Export[filename <> ".emf", picO, ImageResolution -> OptionValue[ImageResolution], Sequence @@ ((# -> OptionValue[#]) & /@ opt)]
     ]
   ]
   ];
