9282c067d76946f1798f5d877c4ccd27c70f4cdc
alex isakov <ale.isakov@gmail.com>
2020-03-30 08:25:05 +0300

    Add files via upload

--- crosssection/CookedLinearModelFit.m
+++ crosssection/CookedLinearModelFit.m
@@ -2,11 +2,29 @@ Package["Economica`"]
 
 CookedLinearModelFit::usage="Simple robust linear regression";
 
-CookedLinearModelFit[l_List] := Module[{x, vars, cooks, clean},
-  vars = Table[Subscript[x, i], {i, Length[First[l]]-1}];
-  cooks = LinearModelFit[l, vars, vars]["CookDistances"];
-  clean = Extract[l, Position[cooks, x_ /; x < 4/Length[l]]];
-  LinearModelFit[clean, vars, vars]
-  ]
+Options[CookedLinearModelFit]={
+	NominalVariables -> None,
+	IncludeConstantBasis -> False};
+
+
+CookedLinearModelFit[l_List,OptionsPattern[]] := Module[{x, vars, cooks, clean,nomVar},
+	vars = Table[Subscript[x, i], {i, Length[First[l]]-1}];
+ 	
+	nomVar = If[Not[(OptionValue@NominalVariables)===None],
+		vars[[OptionValue@NominalVariables]],
+		OptionValue@NominalVariables];
+
+	cooks = LinearModelFit[l, vars, vars,
+  		IncludeConstantBasis->OptionValue@IncludeConstantBasis,
+  		NominalVariables -> nomVar]["CookDistances"];
+  
+	clean = Extract[l, Position[cooks, x_ /; x < 4/Length[l]]];
+  
+  	LinearModelFit[clean, vars, vars,
+  		IncludeConstantBasis->OptionValue@IncludeConstantBasis,
+  		NominalVariables -> nomVar]
+
+ ] 
+
 
 PackageExport["CookedLinearModelFit"]
